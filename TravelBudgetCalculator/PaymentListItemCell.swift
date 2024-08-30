c8e4f5622182a99543e3e5b50eaffe3a544a9bdb
Fukase-Takamasa <ultrafukase@gmail.com>
2024-08-30 19:28:25 +0900

    とりあえず１ジャンル内の入力セルをダミーで表示と、セル上に設置したポップアップボタンの仮実装まで完了。

---
+++ TravelBudgetCalculator/PaymentListItemCell.swift
@@ -0,0 +1,38 @@
+//
+//  PaymentListItemCell.swift
+//  TravelBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 30/8/24.
+//
+
+import UIKit
+
+class PaymentListItemCell: UITableViewCell {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var currencySelectionButton: UIButton!
+    @IBOutlet weak var yenDisplayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupCurrencySelectionButton()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func setupCurrencySelectionButton() {
+        let currencyOptionList = CurrencyType.allCases.map { item in
+            return UIAction(title: item.rawValue, handler: { _ in
+                
+            })
+        }
+        currencySelectionButton.menu = UIMenu(title: "現地通貨を選択", children: currencyOptionList)
+        currencySelectionButton.showsMenuAsPrimaryAction = true
+        currencySelectionButton.changesSelectionAsPrimaryAction = true
+    }
+}
--- TravelBudgetCalculator/ViewController.swift
+++ TravelBudgetCalculator/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum CurrencyType: String, CaseIterable {
+    case euro = "EUR"
+    case usDollar = "USD"
+    // TODO: 通貨ごとのレートは可変なため、enum自体には持たせるべきではないので後から設定できるようにどうにかする
 }
 
+struct PaymentListItem {
+    let title: String
+    let amount: Double
+    let currencyType: CurrencyType
+}
+
+class ViewController: UIViewController {
+    let oneEuroToYenRate: Double = 170.00
+    let oneUsDollarToYenRate: Double = 150.00
+    
+    let section1Items: [PaymentListItem] = [
+        .init(title: "condisででかい水とタキスとチョコクッキー", amount: 4.15, currencyType: .euro),
+        .init(title: "昼のレストランでサラダとパエリアなど", amount: 33.675, currencyType: .euro),
+        .init(title: "タキスとか買ったかも？", amount: 4.94, currencyType: .euro),
+        .init(title: "まりりんたちとご飯", amount: 21.14, currencyType: .euro)
+    ]
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        tableView.register(UINib(nibName: "PaymentListItemCell", bundle: nil), forCellReuseIdentifier: "PaymentListItemCell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section1Items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentListItemCell", for: indexPath) as! PaymentListItemCell
+        let item = section1Items[indexPath.row]
+        cell.titleTextField.text = "\(item.title)"
+        cell.amountTextField.text = "\(item.amount)"
+        
+        let rateValue: Double = {
+            switch item.currencyType {
+            case .euro:
+                return oneEuroToYenRate
+            case .usDollar:
+                return oneUsDollarToYenRate
+            }
+        }()
+        let yenAmount = item.amount * rateValue
+        cell.yenDisplayLabel.text = "\(yenAmount)"
+        return cell
+    }
+}
