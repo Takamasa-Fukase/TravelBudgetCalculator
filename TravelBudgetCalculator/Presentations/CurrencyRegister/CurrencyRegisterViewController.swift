5bb2a81185f89fab8338020817cf385d05c05d6c
Fukase-Takamasa <ultrafukase@gmail.com>
2024-09-06 12:51:14 +0900

    通貨の新規登録画面を作成。作成後の前画面リスト更新も実装

---
+++ TravelBudgetCalculator/Presentations/CurrencyRegister/CurrencyRegisterViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CurrencyRegisterViewController.swift
+//  TravelBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 6/9/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol CurrencyRegisterDelegate: AnyObject {
+    func onRegistered()
+}
+
+class CurrencyRegisterViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    var selectedCurrency: CurrencyType?
+    weak var delegate: CurrencyRegisterDelegate?
+
+    @IBOutlet weak var currencySelectionButton: UIButton!
+    @IBOutlet weak var rateTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCurrencySelectionButton()
+        
+        registerButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else {return}
+                
+                // 通貨が選択されていなかったらエラーを出す
+                guard let selectedCurrency = selectedCurrency else {
+                    self.showError(title: "通貨を選択してください", message: "")
+                    return
+                }
+                
+                // レートが未入力だったらエラーを出す
+                guard let doubleAmount = Double(rateTextField.text ?? "") else {
+                    self.showError(title: "レートの値が不正です", message: "")
+                    return
+                }
+                
+                // 同じ通貨だったらエラーを出す
+                if UserDefaults.registeredCurrencies.contains(where: {
+                    $0.type == selectedCurrency
+                }) {
+                    self.showError(title: "既に登録済みの通貨です", message: "異なる通貨を選択してください。既存の通貨を編集する場合は通貨一覧画面からレートの編集ができます。")
+                    return
+                }
+                
+                let newCurrency = RegisteredCurrency(type: selectedCurrency, toYenRate: doubleAmount)
+                
+                // UserDefaultsに保存
+                UserDefaults.registeredCurrencies = UserDefaults.registeredCurrencies + [newCurrency]
+                
+                // 前の画面のリストを更新させるために通知
+                self.delegate?.onRegistered()
+                
+                // 保存した内容を改めてアラートで表示
+                self.showCompletionAlert(selectedCurrency: selectedCurrency, completion: {
+                    self.dismiss(animated: true)
+                })
+            }).disposed(by: disposeBag)
+    }
+    
+    func setupCurrencySelectionButton() {
+        let currencyOptionList = CurrencyType.allCases.map { item in
+            return UIAction(title: item.rawValue, handler: { [weak self] _ in
+                guard let self = self else { return }
+                // 選択された通貨を変数に保持
+                self.selectedCurrency = item
+            })
+        }
+        currencySelectionButton.menu = UIMenu(title: "通貨を選択", children: currencyOptionList)
+        currencySelectionButton.showsMenuAsPrimaryAction = true
+        currencySelectionButton.changesSelectionAsPrimaryAction = true
+    }
+    
+    func showCompletionAlert(selectedCurrency: CurrencyType, completion: (@escaping () -> Void)) {
+        let alert = UIAlertController(title: "通貨を登録しました", message: "通貨：\(selectedCurrency.description)\nレート：\(rateTextField.text ?? "")円", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "閉じる", style: .default, handler: { _ in
+            completion()
+        })
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
--- TravelBudgetCalculator/Presentations/RateSetting/RateSettingViewController.swift
+++ TravelBudgetCalculator/Presentations/RateSetting/RateSettingViewController.swift
@@ -12,15 +12,16 @@ import RxCocoa
 class RateSettingViewController: UIViewController {
     let disposeBag = DisposeBag()
     var activeTextField = BehaviorRelay<UIView?>(value: nil)
-    var registeredCurrencies: [RegisteredCurrency] = [
-        .init(type: .USD, toYenRate: 0),
-        .init(type: .ARA, toYenRate: 0),
-        .init(type: .COP, toYenRate: 0),
-        .init(type: .MXN, toYenRate: 0),
-        .init(type: .JPY, toYenRate: 0)
-    ]
+//    var registeredCurrencies: [RegisteredCurrency] = [
+//        .init(type: .USD, toYenRate: 0),
+//        .init(type: .ARA, toYenRate: 0),
+//        .init(type: .COP, toYenRate: 0),
+//        .init(type: .MXN, toYenRate: 0),
+//        .init(type: .JPY, toYenRate: 0)
+//    ]
 
     @IBOutlet weak var tableView: TouchesBeganTableView!
+    @IBOutlet weak var toRegisterPageButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,15 @@ class RateSettingViewController: UIViewController {
                               textField: activeTextField.asObservable(),
                               view: view,
                               buttonConstraint: NSLayoutConstraint())
+        
+        toRegisterPageButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else {return}
+                let vc = UIStoryboard(name: "CurrencyRegisterViewController", bundle: nil).instantiateInitialViewController() as! CurrencyRegisterViewController
+                vc.delegate = self
+                vc.modalPresentationStyle = .pageSheet
+                self.present(vc, animated: true)
+            }).disposed(by: disposeBag)
     }
     
     func setupTableView() {
@@ -41,12 +51,12 @@ class RateSettingViewController: UIViewController {
 
 extension RateSettingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return registeredCurrencies.count
+        return UserDefaults.registeredCurrencies.count
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RateSettingCell", for: indexPath) as! RateSettingCell
-        let item = registeredCurrencies[indexPath.row]
+        let item = UserDefaults.registeredCurrencies[indexPath.row]
         // Delegateを親VCに設定
         cell.textField.delegate = self
         
@@ -57,7 +67,10 @@ extension RateSettingViewController: UITableViewDataSource {
             .subscribe(onNext: { [weak self] in
                 guard let self = self else {return}
                 let doubleAmount = Double(cell.textField.text ?? "") ?? 0.0
-                self.registeredCurrencies[indexPath.row].toYenRate = doubleAmount
+//                self.registeredCurrencies[indexPath.row].toYenRate = doubleAmount
+                var editedData = UserDefaults.registeredCurrencies
+                editedData[indexPath.row].toYenRate = doubleAmount
+                UserDefaults.registeredCurrencies = editedData
                 self.tableView.reloadData()
             }).disposed(by: cell.disposeBag)
         
@@ -74,5 +87,11 @@ extension RateSettingViewController: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         activeTextField.accept(textField)
         return true
+    }
+}
+
+extension RateSettingViewController: CurrencyRegisterDelegate {
+    func onRegistered() {
+        self.tableView.reloadData()
     }
 }
