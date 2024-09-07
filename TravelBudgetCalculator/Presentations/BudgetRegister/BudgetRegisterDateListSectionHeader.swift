77906b47fcdb977a1ce8989cd2b9bdca08470490
Fukase-Takamasa <ultrafukase@gmail.com>
2024-09-07 23:35:39 +0900

    予算管理画面のUIが一旦完成

---
+++ TravelBudgetCalculator/Presentations/BudgetRegister/BudgetRegisterDateListSectionHeader.swift
@@ -0,0 +1,20 @@
+//
+//  BudgetRegisterDateListSectionHeader.swift
+//  TravelBudgetCalculator
+//
+//  Created by ウルトラ深瀬 on 7/9/24.
+//
+
+import UIKit
+
+class BudgetRegisterDateListSectionHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- TravelBudgetCalculator/Presentations/BudgetRegister/BudgetRegisterViewController.swift
+++ TravelBudgetCalculator/Presentations/BudgetRegister/BudgetRegisterViewController.swift
@@ -15,29 +15,40 @@ class BudgetRegisterViewController: UIViewController {
     var section1Titles: [String] = ["予算名", "予算金額"]
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "予算を登録"
         setupTableView()
+        
+        registerButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else {return}
+                let selectedIndexPaths = self.tableView.indexPathsForSelectedRows?.filter({ $0.section == 1 }) ?? []
+                
+            }).disposed(by: disposeBag)
     }
     
     func setupTableView() {
         tableView.register(UINib(nibName: "BudgetRegisterInputFormCell", bundle: nil), forCellReuseIdentifier: "BudgetRegisterInputFormCell")
         tableView.register(UINib(nibName: "BudgetRegisterDateListCell", bundle: nil), forCellReuseIdentifier: "BudgetRegisterDateListCell")
-
-//        tableView.register(UINib(nibName: "DateDetailGenreSectionHeader", bundle: nil), forHeaderFooterViewReuseIdentifier: "DateDetailGenreSectionHeader")
+        tableView.register(UINib(nibName: "BudgetRegisterDateListSectionHeader", bundle: nil), forHeaderFooterViewReuseIdentifier: "BudgetRegisterDateListSectionHeader")
         tableView.keyboardDismissMode = .onDrag
         tableView.contentInset.bottom = 200
     }
 }
 
 extension BudgetRegisterViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let sectionHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "DateDetailGenreSectionHeader") as! DateDetailGenreSectionHeader
-//        return sectionHeader
-//    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 1 {
+            let sectionHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "BudgetRegisterDateListSectionHeader") as! BudgetRegisterDateListSectionHeader
+            return sectionHeader
+        }else {
+            return UIView()
+        }
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
@@ -52,19 +63,19 @@ extension BudgetRegisterViewController: UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return UITableView.automaticDimension
-        }else {
-            return UITableView.automaticDimension
-        }
+        return UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if indexPath.section == 0 {
+        if section == 0 {
             return 0.1
         }else {
-            return 48
+            return 36
         }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
